d4b14402727834e4246a63772b8db66307d5683f
Rémi Rollais <remi@nalyze.team>
2023-04-19 15:06:03 +0200

    Feat: Create structure files for todolist

--- todo-list/src/router/index.ts
+++ todo-list/src/router/index.ts
@@ -1,23 +1,25 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// scr/router/index.ts
+
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import Home from '../components/Home.vue'
+import About from '../views/About.vue'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  history: createWebHashHistory(),
+  routes
 })
 
 export default router
